0aee02dfb7d1b9d1d94f13e6edaae0018d86e142
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2022-12-21 22:26:42 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -22,50 +22,55 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: const Text('PontoSolu'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: StaggeredGrid.count(
-          crossAxisCount: 4,
-          mainAxisSpacing: 4,
-          crossAxisSpacing: 4,
-          children: [
-            StaggeredGridTile.count(
-              crossAxisCellCount: 4,
-              mainAxisCellCount: 4,
-              child: Container(
-                color: Colors.red,
-                child: const Text(
-                  'Bater Ponto',
-                  textAlign: TextAlign.center,
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: StaggeredGrid.count(
+              crossAxisCount: 4,
+              mainAxisSpacing: 4,
+              crossAxisSpacing: 4,
+              children: [
+                StaggeredGridTile.count(
+                  crossAxisCellCount: 4,
+                  mainAxisCellCount: 4,
+                  child: Container(
+                    color: Colors.red,
+                    child: const Text(
+                      'Bater Ponto',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            StaggeredGridTile.count(
-              crossAxisCellCount: 2,
-              mainAxisCellCount: 2,
-              child: Container(
-                color: Colors.green,
-                child: const Text(
-                  'marcus',
-                  textAlign: TextAlign.center,
+                StaggeredGridTile.count(
+                  crossAxisCellCount: 2,
+                  mainAxisCellCount: 2,
+                  child: Container(
+                    color: Colors.green,
+                    child: const Text(
+                      'marcus',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            StaggeredGridTile.count(
-              crossAxisCellCount: 2,
-              mainAxisCellCount: 2,
-              child: Container(
-                color: Colors.blue,
-                child: const Text(
-                  'marcus',
-                  textAlign: TextAlign.center,
+                StaggeredGridTile.count(
+                  crossAxisCellCount: 2,
+                  mainAxisCellCount: 2,
+                  child: Container(
+                    color: Colors.blue,
+                    child: const Text(
+                      'marcus',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
