395f0e59316010e65bab48dfb5a02a858e02de91
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2022-12-21 21:55:31 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/pontosolu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pontosolu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('PontoSolu'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: StaggeredGrid.count(
+          crossAxisCount: 4,
+          mainAxisSpacing: 4,
+          crossAxisSpacing: 4,
+          children: [
+            StaggeredGridTile.count(
+              crossAxisCellCount: 4,
+              mainAxisCellCount: 4,
+              child: Container(
+                color: Colors.red,
+                child: const Text(
+                  'Bater Ponto',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            StaggeredGridTile.count(
+              crossAxisCellCount: 2,
+              mainAxisCellCount: 2,
+              child: Container(
+                color: Colors.green,
+                child: const Text(
+                  'marcus',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            StaggeredGridTile.count(
+              crossAxisCellCount: 2,
+              mainAxisCellCount: 2,
+              child: Container(
+                color: Colors.blue,
+                child: const Text(
+                  'marcus',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
