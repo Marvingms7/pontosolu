7909cdc4d9467b0e7ce0da655a30c1d9d9a52e6a
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2023-01-02 12:31:43 -0300

    criando tela de pontos

--- lib/app/pages/pontos.dart
+++ lib/app/pages/pontos.dart
@@ -7,8 +7,20 @@ class Pontos extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(onPressed: () {}, child: const Text('Entrada')),
+              ElevatedButton(onPressed: () {}, child: const Text('Almoço')),
+              ElevatedButton(onPressed: () {}, child: const Text('Retorno')),
+              ElevatedButton(onPressed: () {}, child: const Text('Saida')),
+            ],
+          ),
+        ),
       ),
     );
   }
