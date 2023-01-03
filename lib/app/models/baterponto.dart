ecbdc1cb3712f603a025d5a858bf2c283e426c88
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2023-01-03 16:00:12 -0300

    Update baterponto.dart

--- lib/app/models/baterponto.dart
+++ lib/app/models/baterponto.dart
@@ -1,3 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class DBFirestore {
+  DBFirestore();
+  static final DBFirestore _instance = DBFirestore();
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-
+  static FirebaseFirestore get() {
+    return DBFirestore._instance._firestore;
+  }
+}
