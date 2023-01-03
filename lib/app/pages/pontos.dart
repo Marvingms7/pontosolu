ff15a036b9f2b264ff4a4db7489754266322cb43
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2023-01-03 15:09:22 -0300

    Update pontos.dart

--- lib/app/pages/pontos.dart
+++ lib/app/pages/pontos.dart
@@ -11,113 +11,110 @@ class Pontos extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Flexible(
-                  flex: 1,
-                  child: FractionallySizedBox(
-                    widthFactor: 1,
-                    child: ElevatedButton.icon(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.fromLTRB(
-                          150.0,
-                          50.0,
-                          150.0,
-                          50.0,
-                        ),
-                        backgroundColor: Colors.greenAccent,
-                        elevation: 10,
-                        shadowColor: Colors.greenAccent,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(
+                flex: 1,
+                child: FractionallySizedBox(
+                  widthFactor: 1,
+                  child: ElevatedButton.icon(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.fromLTRB(
+                        150.0,
+                        50.0,
+                        150.0,
+                        50.0,
                       ),
-                      icon: const Icon(
-                        Icons.save,
-                        size: 20,
+                      backgroundColor: Colors.greenAccent,
+                      elevation: 10,
+                      shadowColor: Colors.greenAccent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
                       ),
-                      label: const Text('Ponto'),
                     ),
+                    icon: const Icon(
+                      Icons.save,
+                      size: 20,
+                    ),
+                    label: const Text('Ponto'),
                   ),
                 ),
-                Flexible(
-                  flex: 1,
-                  child: FractionallySizedBox(
-                    widthFactor: 1,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.fromLTRB(
-                          150.0,
-                          50.0,
-                          150.0,
-                          50.0,
-                        ),
-                        elevation: 10,
-                        shadowColor: Colors.blueAccent,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
+              ),
+              Flexible(
+                flex: 1,
+                child: FractionallySizedBox(
+                  widthFactor: 1,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.fromLTRB(
+                        150.0,
+                        50.0,
+                        150.0,
+                        50.0,
                       ),
-                      child: const Text('Almoço'),
+                      elevation: 10,
+                      shadowColor: Colors.blueAccent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
                     ),
+                    child: const Text('Almoço'),
                   ),
                 ),
-                Flexible(
-                  flex: 1,
-                  child: FractionallySizedBox(
-                    widthFactor: 1,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.fromLTRB(
-                          150.0,
-                          50.0,
-                          150.0,
-                          50.0,
-                        ),
-                        backgroundColor: Colors.amberAccent,
-                        elevation: 10,
-                        shadowColor: Colors.amberAccent,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
+              ),
+              Flexible(
+                flex: 1,
+                child: FractionallySizedBox(
+                  widthFactor: 1,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.fromLTRB(
+                        150.0,
+                        50.0,
+                        150.0,
+                        50.0,
                       ),
-                      child: const Text('Retorno'),
+                      backgroundColor: Colors.amberAccent,
+                      elevation: 10,
+                      shadowColor: Colors.amberAccent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
                     ),
+                    child: const Text('Retorno'),
                   ),
                 ),
-                Flexible(
-                  flex: 1,
-                  child: FractionallySizedBox(
-                    widthFactor: 1,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.fromLTRB(
-                          150.0,
-                          50.0,
-                          150.0,
-                          50.0,
-                        ),
-                        backgroundColor: Colors.redAccent,
-                        elevation: 10,
-                        shadowColor: Colors.redAccent,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
+              ),
+              Flexible(
+                flex: 1,
+                child: FractionallySizedBox(
+                  widthFactor: 1,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.fromLTRB(
+                        150.0,
+                        50.0,
+                        150.0,
+                        50.0,
                       ),
-                      child: const Text('Saida'),
+                      backgroundColor: Colors.redAccent,
+                      elevation: 10,
+                      shadowColor: Colors.redAccent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
                     ),
+                    child: const Text('Saida'),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
