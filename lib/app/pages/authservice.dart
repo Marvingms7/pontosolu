11a22b9314f8bdc7a12e175392418ee48c978058
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2023-01-07 11:49:29 -0300

    Create authservice.dart

---
+++ lib/app/pages/authservice.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  User? usuario;
+  bool isLoading = true;
+
+  AuthService() {
+    _authCheck();
+  }
+
+  _authCheck() {
+    _auth.authStateChanges().listen((User? user) {
+      usuario = (user == null) ? null : user;
+      isLoading = false;
+      const NotificationListener(child: Text('sim'));
+    });
+  }
+}
