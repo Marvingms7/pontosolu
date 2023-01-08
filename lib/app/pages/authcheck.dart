648684d65310848fa1a8954abfffe3694780663e
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2023-01-08 16:08:58 -0300

    Athcheck

--- lib/app/pages/authcheck.dart
+++ lib/app/pages/authcheck.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:pontosolu/app/pages/authservice.dart';
+import 'package:pontosolu/app/pages/loginpage.dart';
+import 'package:pontosolu/app/pages/myhomepage.dart';
+import 'package:provider/provider.dart';
 
 class AuthCheck extends StatefulWidget {
   const AuthCheck({super.key});
@@ -12,6 +16,20 @@ class AuthCheck extends StatefulWidget {
 class _AuthCheckState extends State<AuthCheck> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    AuthService auth = Provider.of<AuthService>(context);
+    if (auth.isLoading) {
+      return loading();
+    } else if(auth.usuario == null) {
+      return const LoginPage();
+    } else {
+      return const MyHomePage();
+    }
+  }
+  loading(){
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
   }
 }
