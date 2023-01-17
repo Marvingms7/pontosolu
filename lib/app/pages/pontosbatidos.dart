e6f162438b36919905c1e5763754a267238065e5
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2023-01-17 17:46:55 -0300

    Update pontosbatidos.dart

--- lib/app/pages/pontosbatidos.dart
+++ lib/app/pages/pontosbatidos.dart
@@ -11,19 +11,21 @@ class PontosBatidos extends StatefulWidget {
 class _PontosBatidosState extends State<PontosBatidos> {
   @override
   Widget build(BuildContext context) {
-    final databaseReference = FirebaseDatabase.instance.ref();
-    return StreamBuilder(
-      stream: databaseReference.onValue,
-      builder: (context, snap) {
-        if (snap.hasData && !snap.hasError) {
-          // Use the data to build the UI
-          DataSnapshot snapshot = snap.data!.snapshot;
-          var data = snapshot.value;
-          return Text(data.toString());
-        }
-        // Handle any errors that occurred
-        return Text("Error: ${snap.error}");
-      },
+    final databaseReference = FirebaseDatabase.instance.ref('Usuarios');
+    return Scaffold(
+      body: StreamBuilder(
+        stream: databaseReference.onValue,
+        builder: (context, snap) {
+          if (snap.hasData && !snap.hasError) {
+            // Use the data to build the UI
+            DataSnapshot snapshot = snap.data!.snapshot;
+            var data = snapshot.value;
+            return Text(data.toString());
+          }
+          // Handle any errors that occurred
+          return Text("Error: ${snap.error}");
+        },
+      ),
     );
   }
 }
