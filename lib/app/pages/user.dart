b33e54408df2dea0c8beea73a7c7854252c4f8a9
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2023-01-21 11:22:41 -0300

    Update user.dart

--- lib/app/pages/user.dart
+++ lib/app/pages/user.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:pontosolu/app/pages/loginpage.dart';
 
 class UserName extends ChangeNotifier {
   @override
