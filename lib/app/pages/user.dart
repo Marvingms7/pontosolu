3b924c0d2982fea16227e4066d9bea653cad33a8
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2023-01-21 11:21:29 -0300

    Create user.dart

---
+++ lib/app/pages/user.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:pontosolu/app/pages/loginpage.dart';
+
+class UserName extends ChangeNotifier {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
